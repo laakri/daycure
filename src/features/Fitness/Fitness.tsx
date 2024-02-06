36d1be649abb93274f7e960bae6ddbd3c1965a8d
sheimazb <zbedichaima@gmail.com>
2024-02-06 17:27:37 +0100

    fix(Fitness)
    create ProfilBodyCmponent
    create BMICalculatorCmponent

--- src/features/Fitness/Fitness.tsx
+++ src/features/Fitness/Fitness.tsx
@@ -1,8 +1,20 @@
-const Fitness = () => {
+import BMICalculatorComponent from "./BMICalculatorComponent";
+import React, { StrictMode } from "react";
+import ProfilBodyComponent from "./ProfilBodyComponent";
+import { Flex } from "@chakra-ui/react";
+
+const Fitness: React.FC = () => {
+ 
+
   return (
-    <div>
-      <h2>Fitness Component</h2>
-    </div>
+
+<Flex
+gap={20}
+>
+<ProfilBodyComponent/>
+<BMICalculatorComponent/>
+</Flex>
+
   );
 };
 
