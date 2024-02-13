6a9d860148e9e20e39520c996b2d53209b8a5967
Jelassi Saif <68915309+laakri@users.noreply.github.com>
2024-02-13 19:18:05 +0100

    update the charts

--- src/features/Wallet/Wallet-stats/WalletInfos.tsx
+++ src/features/Wallet/Wallet-stats/WalletInfos.tsx
@@ -1,8 +1,7 @@
-import { Box, Text, Flex } from "@chakra-ui/react";
+import { Box, Text, Flex, useColorModeValue } from "@chakra-ui/react";
 import Chart from "react-apexcharts";
 
 const WalletInfos = () => {
-  // Static local data for demonstration
   const savingsData = {
     labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
     series: [{ name: "Savings", data: [1000, 1200, 800, 1500, 2000, 1800] }],
@@ -16,19 +15,10 @@ const WalletInfos = () => {
       "Utilities",
       "Entertainment",
     ],
-    series: [200, 150, 100, 300, 180], // Dummy values for demonstration
-  };
-
-  const incomeVsExpensesData = {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
-    series: [
-      { name: "Income", data: [3000, 3200, 2800, 3500, 4000, 3800] },
-      { name: "Expenses", data: [1500, 1300, 1800, 2000, 2200, 2000] },
-    ],
+    series: [200, 150, 100, 300, 180],
   };
 
   const options = {
-    // Options for the charts (customize as needed)
     chart: {
       type: "line",
       toolbar: {
@@ -37,7 +27,11 @@ const WalletInfos = () => {
     },
     fill: {
       type: "gradient",
-      colors: ["#4a0ac9", "#bf13d6"],
+    },
+    stroke: {
+      curve: "smooth",
+      width: 5,
+      colors: ["#a94dff", "#a94dff"],
     },
     xaxis: {
       categories: savingsData.labels,
@@ -57,18 +51,13 @@ const WalletInfos = () => {
     grid: {
       show: false,
     },
-    stroke: {
-      curve: "smooth",
-      width: 5,
-      colors: ["#4a0ac9", "#bf13d6"],
-    },
+
     legend: {
       show: false,
     },
   };
 
   const donutOptions = {
-    // Options for the donut chart (customize as needed)
     chart: {
       type: "donut",
       toolbar: {
@@ -77,10 +66,19 @@ const WalletInfos = () => {
     },
     labels: expenseCategoriesData.labels,
     stroke: {
-      width: 0, // Remove border
+      width: 0,
     },
-    colors: ["#4a0ac9", "#8B5CF6", "#D1D5DB", "#9CA3AF", "#E5E7EB"], // Custom colors for donut segments
+    colors: [
+      "#530087",
+      "#60009c",
+      "#6e00b3",
+      "#8e00ed",
+      "#E5E7EB",
+      "#9a00fa",
+      "#9900ff",
+    ],
   };
+  const textColor = useColorModeValue("black", "white");
 
   return (
     <Flex flexDirection={"column"} w={"100%"} gap={3}>
@@ -99,6 +97,7 @@ const WalletInfos = () => {
             series={savingsData.series}
             type="line"
             width="100%"
+            color={textColor}
           />
         </Box>
         <Box
@@ -115,26 +114,10 @@ const WalletInfos = () => {
             series={expenseCategoriesData.series}
             type="donut"
             width="100%"
+            color={textColor}
           />
         </Box>
       </Flex>
-      <Box
-        width={"100%"}
-        bg={"var(--lvl1-darkcolor)"}
-        border={"var(--bordercolor) solid 1px"}
-        rounded={10}
-      >
-        <Text fontWeight="bold" m={4}>
-          Income vs Expenses Comparison
-        </Text>
-        <Chart
-          options={options}
-          series={incomeVsExpensesData.series}
-          type="bar"
-          width="100%"
-          height={280}
-        />
-      </Box>
     </Flex>
   );
 };
