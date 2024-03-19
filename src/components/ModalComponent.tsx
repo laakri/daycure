9886dea751621e4932284232a03b17f1ad3c47e5
Jelassi Saif <68915309+laakri@users.noreply.github.com>
2024-03-19 12:18:24 +0100

    update the Quick Search Modal

--- src/components/ModalComponent.tsx
+++ src/components/ModalComponent.tsx
@@ -12,9 +12,12 @@ import {
   Kbd,
   useToast,
   Button,
+  Wrap,
 } from "@chakra-ui/react";
 import { BiSend } from "react-icons/bi";
 import { addTask } from "../states/schedule";
+import { addTransaction } from "../states/wallet";
+import { listcategories } from "../features/Wallet/CategoriesIcons";
 
 interface ModalComponentProps {
   isOpen: boolean;
@@ -24,14 +27,20 @@ interface ModalComponentProps {
 const ModalComponent: React.FC<ModalComponentProps> = ({ isOpen, onClose }) => {
   const [searchText, setSearchText] = useState("");
   const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
   const [isImportant, setIsImportant] = useState(false);
   const [date, setDate] = useState<Date | undefined>(undefined);
+  const [amount, setAmount] = useState(0);
+  const [isExpense, setIsExpense] = useState(false);
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.ctrlKey && event.key === "e") {
         event.preventDefault();
         setSearchText("Task :");
+      } else if (event.ctrlKey && event.key === "q") {
+        event.preventDefault();
+        setSearchText("Wallet :");
       }
     };
 
@@ -72,10 +81,58 @@ const ModalComponent: React.FC<ModalComponentProps> = ({ isOpen, onClose }) => {
         return nextDay;
       }
     }
-    // If no matching expression is found, return the current date
     return now;
   };
+  const isTransactionExpense = (description: string): boolean => {
+    const expenseKeywords = ["expense", "spent", "bought", "cost", "purchase"];
 
+    const incomeKeywords = [
+      "income",
+      "earned",
+      "received",
+      "salary",
+      "payment",
+    ];
+
+    const hasExpenseKeyword = expenseKeywords.some((keyword) =>
+      description.toLowerCase().includes(keyword)
+    );
+
+    const hasIncomeKeyword = incomeKeywords.some((keyword) =>
+      description.toLowerCase().includes(keyword)
+    );
+
+    if (hasExpenseKeyword && !hasIncomeKeyword) {
+      return true;
+    }
+
+    if (hasIncomeKeyword && !hasExpenseKeyword) {
+      return false;
+    }
+
+    return true;
+  };
+  const handleCommand = (command: string) => {
+    const commands: any = {
+      Task: {
+        text: "Task :",
+        pageUrl: "http://localhost:5173/schedule",
+      },
+      Wallet: {
+        text: "Wallet :",
+        pageUrl: "http://localhost:5173/schedule",
+      },
+      // Add more commands here if necessary
+    };
+
+    const selectedCommand = commands[command];
+    if (selectedCommand) {
+      setSearchText(selectedCommand.text);
+      if (selectedCommand.pageUrl) {
+        window.location.href = selectedCommand.pageUrl;
+      }
+    }
+  };
   const handleInputChange = (value: string) => {
     setSearchText(value);
     if (value.startsWith("Task :")) {
@@ -106,12 +163,39 @@ const ModalComponent: React.FC<ModalComponentProps> = ({ isOpen, onClose }) => {
             /\b(important|urgent|critical|priority)\b/i.test(part)
           )
         );
-      } else {
-        // Optionally, provide feedback to the user about empty task description
-        console.log("Task description cannot be empty");
+      }
+    } else if (value.startsWith("Wallet :")) {
+      const transactionInput = value.slice("Wallet :".length).trim();
+
+      // Regular expression to match currency (with optional symbol)
+      const amountRegex = /(\d+(\.\d+)?)\s*([a-zA-Z]+|\$)?/;
+      const amountMatch = transactionInput.match(amountRegex);
+
+      if (amountMatch) {
+        const amount = parseFloat(amountMatch[1]);
+        const currencySymbol = amountMatch[3] || ""; // Get currency symbol or empty string
+        const description = transactionInput.replace(amountRegex, "").trim();
+        const isExpense = isTransactionExpense(description);
+        const categorys =
+          listcategories.find((cat) =>
+            description.toLowerCase().includes(cat.toLowerCase())
+          ) || "Other";
+
+        // Update state variables
+        setAmount(amount);
+        setDescription(description);
+        setIsExpense(isExpense);
+        setCategory(categorys);
+
+        console.log("Amount:", amount);
+        console.log("Currency Symbol:", currencySymbol);
+        console.log("Description:", description);
+        console.log("Is Expense:", isExpense);
+        console.log("Category:", category);
       }
     }
   };
+
   const toast = useToast();
 
   const sendQuickEntry = async () => {
@@ -144,9 +228,55 @@ const ModalComponent: React.FC<ModalComponentProps> = ({ isOpen, onClose }) => {
           isClosable: true,
         });
       }
-    } else {
-      // Handle other types of quick entries here
-      console.log("Quick entry of different type");
+    } else if (searchText.startsWith("Wallet :")) {
+      try {
+        console.log("amount", amount, "description", description);
+        if (!isNaN(amount)) {
+          if (isExpense) {
+            await addTransaction({
+              amount,
+              date: new Date(),
+              description,
+              isExpense: true,
+              userId: "65b0320bb3870b156e159462",
+              category: "",
+            });
+            setSearchText("");
+            toast({
+              title: "Expense transaction added successfully!",
+              status: "success",
+              duration: 2000,
+              isClosable: true,
+            });
+          } else {
+            await addTransaction({
+              amount,
+              date: new Date(),
+              description,
+              isExpense: false,
+              userId: "65b0320bb3870b156e159462",
+              category: "",
+            });
+            setSearchText("");
+            toast({
+              title: "Income transaction added successfully!",
+              status: "success",
+              duration: 2000,
+              isClosable: true,
+            });
+          }
+        } else {
+          console.log("Invalid wallet transaction input");
+        }
+      } catch (error) {
+        toast({
+          title: "Error adding income transaction",
+          status: "error",
+          duration: 2000,
+          isClosable: true,
+        });
+        console.error("Error adding income transaction", error);
+      }
     }
   };
 
@@ -198,70 +328,142 @@ const ModalComponent: React.FC<ModalComponentProps> = ({ isOpen, onClose }) => {
               )}
             </Text>
           </Flex>
+          <Flex justifyContent="space-between" alignItems="center" m={2}>
+            <Text fontSize="sm" color="gray.500">
+              {searchText.includes("Wallet :") && (
+                <>
+                  Amount: {amount} {isExpense ? "Expense" : "Income"}
+                </>
+              )}
+            </Text>
+          </Flex>
+
+          {/* Custom Commands */}
           <Flex flexDirection={"column"} gap={2} mt={2}>
+            <Flex flexDirection={"column"} gap={2} mt={2}>
+              <Flex
+                justifyContent={"space-between"}
+                bg={"gray.800"}
+                p={" 10px 15px"}
+                rounded={5}
+              >
+                <Text color={"gray.300"}>
+                  You can add a task by typing "Task :"
+                </Text>
+
+                <Flex alignItems={"center"} gap={2}>
+                  <Text fontSize={"xs"} color={"purple.300"}>
+                    or
+                  </Text>
+                  <Text>
+                    <Kbd
+                      bg={"var(--lvl3-darkcolor)"}
+                      border="var(--bordercolor) solid 1px"
+                      mr={1}
+                    >
+                      Ctrl
+                    </Kbd>
+                    +
+                    <Kbd
+                      bg={"var(--lvl3-darkcolor)"}
+                      border="var(--bordercolor) solid 1px"
+                    >
+                      E
+                    </Kbd>
+                  </Text>
+                </Flex>
+              </Flex>{" "}
+              <Flex
+                justifyContent={"space-between"}
+                bg={"gray.800"}
+                p={" 10px 15px"}
+                rounded={5}
+              >
+                <Text color={"gray.300"}>
+                  You can add a Transaction by typing "Wallet :"
+                </Text>
+
+                <Flex alignItems={"center"} gap={2}>
+                  <Text fontSize={"xs"} color={"purple.300"}>
+                    or
+                  </Text>
+                  <Text>
+                    <Kbd
+                      bg={"var(--lvl3-darkcolor)"}
+                      border="var(--bordercolor) solid 1px"
+                      mr={1}
+                    >
+                      Ctrl
+                    </Kbd>
+                    +
+                    <Kbd
+                      bg={"var(--lvl3-darkcolor)"}
+                      border="var(--bordercolor) solid 1px"
+                    >
+                      Q
+                    </Kbd>
+                  </Text>
+                </Flex>
+              </Flex>
+            </Flex>
+
+            {/* Recent History */}
+            <Flex
+              flexDirection={"column"}
+              bg={"gray.800"}
+              p={"10px 15px"}
+              rounded={5}
+            >
+              <Text color={"gray.300"}>Recent History:</Text>
+              <Wrap mt={2} spacing={"3px"}>
+                <Text
+                  fontSize={"sm"}
+                  color={"gray.300"}
+                  bg={"var(--lvl3-darkcolor)"}
+                  px={2}
+                  pb={1}
+                  rounded={5}
+                >
+                  Task Page
+                </Text>
+                <Text
+                  fontSize={"sm"}
+                  color={"gray.300"}
+                  bg={"var(--lvl3-darkcolor)"}
+                  px={2}
+                  pb={1}
+                  rounded={5}
+                >
+                  Fitness
+                </Text>
+                <Text
+                  fontSize={"sm"}
+                  color={"gray.300"}
+                  bg={"var(--lvl3-darkcolor)"}
+                  px={2}
+                  pb={1}
+                  rounded={5}
+                >
+                  Wallet
+                </Text>
+              </Wrap>
+            </Flex>
+
+            {/* Contextual Suggestions */}
             <Flex
               justifyContent={"space-between"}
               bg={"gray.800"}
-              p={" 10px 15px"}
+              p={"10px 15px"}
               rounded={5}
             >
-              <Text color={"gray.300"}>
-                You can add a task by typing "Task :"
-              </Text>
-
-              <Flex alignItems={"center"} gap={2}>
-                <Text fontSize={"xs"} color={"purple.300"}>
-                  or
-                </Text>
-                <Text>
-                  <Kbd
-                    bg={"var(--lvl3-darkcolor)"}
-                    border="var(--bordercolor) solid 1px"
-                    mr={1}
-                  >
-                    Ctrl
-                  </Kbd>
-                  +
-                  <Kbd
-                    bg={"var(--lvl3-darkcolor)"}
-                    border="var(--bordercolor) solid 1px"
-                  >
-                    E
-                  </Kbd>
-                </Text>
-              </Flex>
-            </Flex>{" "}
-            <Flex
-              justifyContent={"space-between"}
-              bg={"gray.800"}
-              p={" 10px 15px"}
-              rounded={5}
-            >
-              <Text color={"gray.300"}>
-                You can add a Transaction by typing "Wallet :"
-              </Text>
-
-              <Flex alignItems={"center"} gap={2}>
-                <Text fontSize={"xs"} color={"purple.300"}>
-                  or
-                </Text>
-                <Text>
-                  <Kbd
-                    bg={"var(--lvl3-darkcolor)"}
-                    border="var(--bordercolor) solid 1px"
-                    mr={1}
-                  >
-                    Ctrl
-                  </Kbd>
-                  +
-                  <Kbd
-                    bg={"var(--lvl3-darkcolor)"}
-                    border="var(--bordercolor) solid 1px"
-                  >
-                    Q
-                  </Kbd>
-                </Text>
-              </Flex>
+              <Text color={"gray.300"}>Contextual Suggestions:</Text>
+              <Button
+                size="sm"
+                colorScheme="white"
+                border={"solid 1px var(--bordercolor)"}
+              >
+                Show Suggestions
+              </Button>
             </Flex>
           </Flex>
         </ModalBody>
